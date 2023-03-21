884aff50638ff3c2812cf391b6ae8a7dfe31f0b5
tan <buitan561993@gmail.com>
2023-03-21 09:02:09 +0700

    update

--- MD3_Bai10/BaiTap/src/Exercise1/MyList.java
+++ MD3_Bai10/BaiTap/src/Exercise1/MyList.java
@@ -8,6 +8,18 @@ public class MyList {
     Object elements[];
 
     public MyList() {
+        elements=new Object[DEFAULT_CAPACITY];
     };
 
+    public MyList(int capacity) {
+        elements = new Object[capacity];
+    }
+    public E remove(int index){
+        if (index>=size||index<0){
+            throw new IndexOutOfBoundsException("Index : " +index + ", Size :"+index);
+        }
+        Object temp=elements[index];
+        for (int i =index;i<elements.length-1; i++){
+        elements[i]=elements[i+1];
+        }
 }
